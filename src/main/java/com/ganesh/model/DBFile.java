f770acaf8d2e2f231a5f1729ac8ba96aaa78cc4f
Ganesh Kumar <47558133+Ganeshk750@users.noreply.github.com>
2021-04-27 12:42:41 +0530

    # Adding New DbFile Class

---
+++ src/main/java/com/ganesh/model/DBFile.java
@@ -0,0 +1,68 @@
+package com.ganesh.model;
+
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+/**
+ * @created: 27/04/2021 - 12:36 PM
+ * @author: Ganesh
+ */
+
+@Entity
+@Table(name = "files")
+public class DBFile {
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name="uuid", strategy = "uuid2")
+    private String id;
+
+    private String fileName;
+
+    private String fileType;
+
+    @Lob
+    private byte[] data;
+
+
+    public DBFile(){ }
+
+    public DBFile(String fileName, String fileType, byte[] data) {
+        this.fileName = fileName;
+        this.fileType = fileType;
+        this.data = data;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFileType() {
+        return fileType;
+    }
+
+    public void setFileType(String fileType) {
+        this.fileType = fileType;
+    }
+
+    public byte[] getData() {
+        return data;
+    }
+
+    public void setData(byte[] data) {
+        this.data = data;
+    }
+}
